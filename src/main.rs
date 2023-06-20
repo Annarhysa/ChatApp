58580c2a6a83968d89b3878a656618f8e6682362
Annarhysa Albert <annarhysa13@gmail.com>
2023-06-20 21:34:48 +0530

    New changes in toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main(){
-  println!("ChatAPP");
+#[macro_use] extern crate rocket;
+
+#[get("/world")]
+fn world() -> &'static str{
+  "Hello, world!"
 }
+
+#[launch]
+fn rocket() -> _{
+  rocket::build(): Rocket<Build>.mount(base: "/hello", routes :routes![world])
+
+}
+  
